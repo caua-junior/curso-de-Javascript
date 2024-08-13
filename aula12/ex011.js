cf7809a145f3fe5686ac0f46c253009a3bcccfd1
cauan-junior <vitincaua@gmail.com>
2024-08-13 15:01:19 +0100

    att

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,12 +1,9 @@
-var idade = 20
+var idade = 66
+console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
     console.log("Nao vota")
-} else {
-    if (idade >= 16 && idade < 18) {
+} else if (idade < 18 || idade > 65) {
         console.log("Voto opcional")
-    }else{
-       if (idade > 18){
+    }else {
             console.log("Voto obrigatorio")
-        }
     }
-}
