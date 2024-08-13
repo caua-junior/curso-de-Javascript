1d03627e5b3bee4987866ecf74c48f92f347ef8f
cauan-junior <vitincaua@gmail.com>
2024-08-13 14:04:25 +0100

    att curso js.

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){
+    console.log(`voce ultrapassou a velocidade.MULTADO!`)
+}
+console.log(`dirija usando cinto de segurança`)
---
+++ aula12/011.js
@@ -0,0 +1,12 @@
+var idade = 20
+if (idade < 16) {
+    console.log("Nao vota")
+} else {
+    if (idade >= 16 && idade < 18) {
+        console.log("Voto opcional")
+    }else{
+       if (idade > 18){
+            console.log("Voto obrigatorio")
+        }
+    }
+}
